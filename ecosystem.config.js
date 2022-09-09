5b447c31ed776afcc3794bfa09a4fb0a6c4326bd
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-09-09 20:15:40 +0200

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,17 @@ module.exports = {
       name: "execs",
       script: "./exec-server/index.js",
       args: '-exep 7070 -webp 6060',
-      watch: true,
-      // Delay between restart
-      watch_delay: 1000,
-      ignore_watch: ["node_modules"],
+      watch: ["exec-server"],,
+    // Delay between restart
+    watch_delay: 1000,
+    ignore_watch: ["node_modules"],
     },
-    {
-      name: "mains",
-      script: "./main-server/main-server.js",
-      watch: ["./main-server"],
-      // Delay between restart
-      watch_delay: 1000,
-      ignore_watch: ["node_modules"],
-    }]
+{
+  name: "mains",
+  script: "./main-server/main-server.js",
+  watch: ["main-server"],
+  // Delay between restart
+  watch_delay: 1000,
+  ignore_watch: ["node_modules"],
+}]
 }
