b0b7acfa4abe12ebf14e9f4a53e854f0c9904051
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-09-09 14:30:12 +0200

    +

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,12 @@ module.exports = {
   apps: [
     {
       name: "execs",
-      script: "./exec-server/index.js"
+      script: "./exec-server/index.js",
+      args: '-exep 1234 -webp 6060',
     },
     {
       name: "mains",
       script: "./main-server/main-server.js"
+
     }]
 }
--- gitsshpullForward.bat
+++ gitsshpullForward.bat
@@ -1,6 +1,6 @@
 ssh -t -p 3164 sr-projekt-load-balancer@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull"
 
-ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull && ~/node_modules/pm2/bin/pm2 start ~/mys-projekt-load-balancer-auto-scaler/ecosystem.config.js --only "execs""
+ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull && ~/node_modules/pm2/bin/pm2 start ~/mys-projekt-load-balancer-auto-scaler/ecosystem.config.js --only "execs" --node-args="dev --exep=1234""
 
 ssh -t -p 3182 sr-projekt-execs-2@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull"
 
@@ -11,4 +11,4 @@ ssh -t -p 3172 sr-projekt-execs-4@h2899502.stratoserver.net "cd ~/mys-projekt-lo
 
 rem ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "~/node_modules/pm2/bin/pm2 list"
 
-ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "ls ~/mys-projekt-load-balancer-auto-scaler/"
+rem ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "ls ~/mys-projekt-load-balancer-auto-scaler/"
