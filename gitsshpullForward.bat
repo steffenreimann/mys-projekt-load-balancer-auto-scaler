054262ecfaf1d9b318a7ff2d0a489347c3e671fa
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-09-09 20:07:06 +0200

    +

--- gitsshpullForward.bat
+++ gitsshpullForward.bat
@@ -4,7 +4,7 @@ rem ssh -t -p 3164 sr-projekt-load-balancer@h2899502.stratoserver.net "cd ~/mys-
 
 rem ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull && ~/node_modules/pm2/bin/pm2 start ~/mys-projekt-load-balancer-auto-scaler/ecosystem.config.js --only "execs""
 
-rem ssh -t -p 3182 sr-projekt-execs-2@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull && ~/node_modules/pm2/bin/pm2 start ~/mys-projekt-load-balancer-auto-scaler/ecosystem.config.js --only "execs""
+ssh -t -p 3182 sr-projekt-execs-2@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull && ~/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 start ~/mys-projekt-load-balancer-auto-scaler/ecosystem.config.js --only "execs""
 
 rem ssh -p 3182 sr-projekt-execs-2@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler; sh gitsshpullLocal.sh"
 
@@ -19,4 +19,4 @@ rem ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "ls ~/mys-projek
 rem ssh -t -p 3164 sr-projekt-load-balancer@h2899502.stratoserver.net "ls ~/.nvm/versions/node/v18.0.0/lib/node_modules/pm2/bin/pm2 list"
 
 rem ssh -p 3182 sr-projekt-execs-2@h2899502.stratoserver.net "echo ~/mys-projekt-load-balancer-auto-scaler/gitsshpullLocal.sh" 
-ssh -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "bash ~/mys-projekt-load-balancer-auto-scaler/gitsshpullLocal.sh" 
+rem ssh -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "bash ~/mys-projekt-load-balancer-auto-scaler/gitsshpullLocal.sh" 
