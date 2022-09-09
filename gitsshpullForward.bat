afdd8f56c310b3fd66662acf581a26037aefd153
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-09-09 13:22:21 +0200

    +

--- gitsshpullForward.bat
+++ gitsshpullForward.bat
@@ -1,5 +1,3 @@
-#ssh -p 3164 sr-projekt-load-balancer@h2899502.stratoserver.net "cd /mys-projekt-load-balancer-auto-scaler ; bash --login; git reset --hard HEAD && git pull"
-
 ssh -t -p 3164 sr-projekt-load-balancer@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull"
 
 ssh -t -p 3154 sr-projekt-execs-1@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull"
@@ -9,4 +7,3 @@ ssh -t -p 3182 sr-projekt-execs-2@h2899502.stratoserver.net "cd ~/mys-projekt-lo
 ssh -t -p 3151 sr-projekt-execs-3@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull"
 
 ssh -t -p 3172 sr-projekt-execs-4@h2899502.stratoserver.net "cd ~/mys-projekt-load-balancer-auto-scaler ; git reset --hard HEAD && git pull"
-
